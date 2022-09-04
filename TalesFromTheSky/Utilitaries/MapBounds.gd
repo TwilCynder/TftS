639498a4df0ba312523d16e4aaf34b96340c03c8
TwilCynder <twilcynder@gmail.com>
2022-09-04 17:12:21 +0200

    mapbounds

--- TalesFromTheSky/Utilitaries/MapBounds.gd
+++ TalesFromTheSky/Utilitaries/MapBounds.gd
@@ -4,7 +4,12 @@ extends "res://Utilitaries/EditorOnly.gd"
 
 class_name MapBounds
 
-export (Vector2) var size = Vector2(320, 240) setget onset
+var camW = 320
+var camH = 240
+export (Vector2) var size = Vector2(camW, camH) setget onset
+
+var color = Color(255, 0, 0)
+
 
 onready var rect = Rect2(Vector2(0, 0), size)
 # Called when the node enters the scene tree for the first time.
@@ -17,5 +22,14 @@ func onset(s: Vector2):
 	pass
 
 func _draw():
-	draw_rect(rect, Color(255, 0, 0), false, 3)
-	pass
+	draw_rect(rect, color, false, 3)
+	
+	var x: int = camW
+	while (x < size.x):
+		draw_line(Vector2(x, 0), Vector2(x, size.y), color, 1.5)
+		x += camW
+	
+	var y: int = camH
+	while (y < size.y):
+		draw_line(Vector2(0, y), Vector2(size.x, y), color, 1.5)
+		y+= camH
