e772b012643590053ab0a3ceb861a05ba579b62c
TwilCynder <twilcynder@gmail.com>
2022-09-11 11:54:00 +0200

    generic animated effect script

---
+++ TalesFromTheSky/Effects/AnimatedEffect.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite
+
+class_name AnimatedEffect
+
+func _ready():
+	connect("animation_finished", self, "_on_animation_finished")
+	play("Animate")
+
+func _on_animation_finished():
+	queue_free()
