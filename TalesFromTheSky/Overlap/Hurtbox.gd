9d3bbe494a84610323d6d0463f144ce8a7dbfb6d
TwilCynder <twilcynder@gmail.com>
2022-09-11 12:42:59 +0200

    hurtbox hit effect

--- TalesFromTheSky/Overlap/Hurtbox.gd
+++ TalesFromTheSky/Overlap/Hurtbox.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
-signal hit(hitbox)
+class_name Hurtbox
+
+signal hit(hitbox, hurtbox)
+
+export (bool) var enable_sparkle = false
+
+var HitEffect = preload("res://Effects/Hit.tscn")
 
 func _on_Hurtbox_area_entered(area):
 	if (area is Hitbox):
-		emit_signal("hit", area as Hitbox)
+		var effect = HitEffect.instance()
+		self.add_child(effect)
+		emit_signal("hit", area as Hitbox, self)
