cc6899574f0559bfc635ce2307ac782da277c5af
TwilCynder <twilcynder@gmail.com>
2022-10-11 22:08:56 +0200

    player knockback + player script overhaul

--- TalesFromTheSky/Overlap/Hitbox.gd
+++ TalesFromTheSky/Overlap/Hitbox.gd
@@ -9,15 +9,19 @@ enum HitboxType {
 }
 
 export (int) var damage = 1
+export(HitboxType) var hitboxType = HitboxType.NONE
 
 var knockback = null
 
-export(HitboxType) var hitboxType = HitboxType.NONE
+onready var collisionShape: CollisionShape2D = $CollisionShape2D
 
-func computeEffectiveKnockback(posTarget: Vector2):
+func set_enabled(state: bool):
+	collisionShape.disabled = state
+
+func computeEffectiveKnockback(posTarget: Vector2) -> Vector2:
 	if (knockback is Vector2):
 		return knockback
-	elif (knockback is int):
-		return (position - posTarget) * (knockback / position.distance_to(posTarget))
+	elif (typeof(knockback) == TYPE_REAL):
+		return (posTarget - global_position) * (knockback / global_position.distance_to(posTarget))
 	else:
 		return Vector2.ZERO 
--- TalesFromTheSky/Overlap/Hurtbox.gd
+++ TalesFromTheSky/Overlap/Hurtbox.gd
@@ -6,6 +6,8 @@ signal hit(hitbox, hurtbox)
 
 export (bool) var enable_sparkle = false
 
+export (Hitbox.HitboxType) var hit_by = Hitbox.HitboxType.NONE
+
 var HitEffect = preload("res://Effects/Hit.tscn")
 
 func _getHitEffect() -> AnimatedEffect:
@@ -17,8 +19,16 @@ static func computeHitPosition(hit: Area2D, hurt: Area2D) -> Vector2:
 func _displayEffect(hitbox: Hitbox):
 	AnimatedEffect.displayEffect(_getHitEffect(), computeHitPosition(hitbox, self) )
 
+func collision(hitbox: Hitbox):
+	_displayEffect(hitbox)
+	emit_signal("hit", hitbox, self)
+
+func checkHit(hitbox: Hitbox):
+	
+	if (hitbox.hitboxType == hit_by):
+		collision(hitbox)
+
 func _on_Hurtbox_area_entered(area):
 	if (area is Hitbox):
-		_displayEffect(area)
-		emit_signal("hit", area as Hitbox, self)
-
+		print("collision")
+		checkHit(area as Hitbox)
