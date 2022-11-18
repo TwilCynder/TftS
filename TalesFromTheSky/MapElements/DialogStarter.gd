6ca525f54c733edfeddfc6287ce680450521d53e
TwilCynder <twilcynder@gmail.com>
2022-11-18 23:42:12 +0100

    dialogs + npcs

---
+++ TalesFromTheSky/MapElements/DialogStarter.gd
@@ -0,0 +1,26 @@
+tool
+
+extends Node
+
+class_name DialogStarter
+
+export (String, FILE, "*.json") var path = ""
+export (String) var id = "" 
+
+func _get_configuration_warning() -> String:
+	if path == "":
+		return "Empty dialog path"
+	else:
+		return ""
+
+	if id == "":
+		return "Empty dialog ID"
+	else:
+		return ""
+
+func _startDialog():
+	DialogBox.startDialog(path, id)
+
+
+func _on_LookInteractionBox_hero_interact(hero):
+	_startDialog()
--- TalesFromTheSky/UI/DialogBox.gd
+++ TalesFromTheSky/UI/DialogBox.gd
@@ -1,22 +1,50 @@
 extends ColorRect
 
 export var dialogPath = ""
-export (float) var textSpeed = 0.05
+export var dialogId = ""
+export (float) var baseTextSpeed = 0.05
 
-var dialog
+var dialog: Array
 
 var phraseNum = 0
 var finished = false
 
 func _ready():
-	$Timer.wait_time = textSpeed
-	dialog = get_dialog()
+	self.visible = false
+	pass
 
-func get_dialog() -> Array:
+func startDialog(path: String, id: String, textSpeed: float = -1):
+	if textSpeed == -1:
+		textSpeed = baseTextSpeed
+	$Timer.wait_time = textSpeed
+	dialog = get_dialog(path, id)
+	self.visible = true
+
+func get_dialog(path: String, id: String) -> Array:
+	
+	var f = File.new()
+	assert(f.file_exists(dialogPath), "Dialog path does not exist")
+	
+	f.open(dialogPath, File.READ)
+	var json = f.get_as_text()
+	var dialogs = JSON.parse(json).result
+	
+	if typeof(dialogs) == TYPE_DICTIONARY:
+		print("Ma bite")
+	
 	return []
+	
+func _process(delta):
+	$Indicator.visible = finished
+	if Input.is_action_just_pressed("ui_accept"):
+		if finished:
+			nextPhrase()
+		else:
+			$Text.visible_characters = len($Text.text)
 
 func nextPhrase():
 	if (phraseNum >= len(dialog)):
+		self.visible = false
 		return
 
 	$Name.bbcode_text = dialog[phraseNum]["Name"]
