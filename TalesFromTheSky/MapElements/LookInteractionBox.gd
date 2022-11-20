0eb00cae414e4b4999833e415d3c9964720db98a
TwilCynder <twilcynder@gmail.com>
2022-11-20 17:55:25 +0100

    fox lookinteractionbox

--- TalesFromTheSky/MapElements/LookInteractionBox.gd
+++ TalesFromTheSky/MapElements/LookInteractionBox.gd
@@ -3,5 +3,5 @@ extends InteractionBox
 class_name LookInteractionBox
 
 func _on_hero_interact(hero: Player):
-	if hero.is_looking_towards(self.position):
+	if hero.is_looking_towards(self.global_position):
 		._on_hero_interact(hero)
