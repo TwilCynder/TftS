da92f5ec8c496e29f2521fef48a4c8bc9e852000
Nillan <nillan.eldaar@lilo.org>
2022-12-03 12:16:20 +0100

    Dialogs
    ET CA MARCHE PAS

--- TalesFromTheSky/Utilitaries/FontManager/Configurator.gd
+++ TalesFromTheSky/Utilitaries/FontManager/Configurator.gd
@@ -4,6 +4,13 @@ extends FontConfigurator
 
 func init():
 	largeur_caractere("I", 3)
+	largeur_caractere("i", 3)
+	largeur_caractere("j", 4)
+	largeur_caractere("l", 3)
+	largeur_caractere("t", 5)
+	largeur_caractere("1", 5)
+	largeur_caractere(".", 3)
+	largeur_caractere(",", 3)
 	
 	
 func _ready():
