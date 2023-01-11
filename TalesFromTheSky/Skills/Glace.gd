037ee2bf5797141ff6f2fe25e39918399f5b520b
TwilCynder <twilcynder@gmail.com>
2023-01-11 18:49:13 +0100

    spell use progress

--- TalesFromTheSky/Skills/Glace.gd
+++ TalesFromTheSky/Skills/Glace.gd
@@ -1,15 +1,20 @@
 extends Skill
 
 var IceEffect: Resource = load("res://Skills/Effects/Ice/IceEffect.tscn")
+var IceBlock: Resource = load("res://Skills/Effects/Ice/IceBlock.tscn")
 
 func _ready():
 	print("Ice ready !")
 		
-func use(player: Player):
-	print("Ice used !")
+func display_effect(player: Player):
 	var position: Vector2 = player.direction.normalized() * 16
 	var effect = player.effect_manager.add_effect(IceEffect, position)
-	effect.connect("effect_end", self, "_on_finished", [player])
+	effect.connect("effect_end", self, "_on_effect_finished", [player])
+		
+func use(player: Player):
+	print("Ice used !")
+	display_effect(player)
 	
-func _on_finished(player: Player):
+func _on_effect_finished(player: Player):
+	var block = IceBlock.instance()
 	player.end_skill()
